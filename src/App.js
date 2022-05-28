f071103e6791e7b80dd16e91d8c66fb425694d2b
Damjan15 <stamenovdamjan20@gmail.com>
2022-05-28 01:47:51 -0700

    Features section created

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import { Hero } from "./components";
+import { Features, Hero } from "./components";
 
 function App() {
   return (
     <div>
       <Hero />
+      <Features />
     </div>
   );
 }
